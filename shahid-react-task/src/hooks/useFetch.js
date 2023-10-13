61796e2dc21d85aaec9ea0893e21fb74aa7afdc9
Abdallah Marei <90200082+AbdallahMarei@users.noreply.github.com>
2023-10-13 21:18:39 +0300

    Update useFetch.js

--- shahid-react-task/src/hooks/useFetch.js
+++ shahid-react-task/src/hooks/useFetch.js
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 
 const useFetch = (endpoint) => {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const response = await fetch(endpoint);
                 const data = await response.json();
                 setData(data.results);
+                setLoading(false);
             } catch(error) {
                 console.error(error);
             }
@@ -15,7 +18,7 @@ const useFetch = (endpoint) => {
         fetchMovies();
     }, [endpoint]);
 
-    return data;
+    return {data, loading};
 }
 
 export default useFetch;
