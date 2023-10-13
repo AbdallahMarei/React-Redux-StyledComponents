83f79d02fdfbe9cb9be2b8728bfded82bb92669b
Abdallah Marei <90200082+AbdallahMarei@users.noreply.github.com>
2023-10-13 21:19:00 +0300

    Update BlogPage.jsx

--- shahid-react-task/src/pages/BlogPage.jsx
+++ shahid-react-task/src/pages/BlogPage.jsx
@@ -5,16 +5,18 @@ import { StyledTitle } from '../components/styled/Title.styled';
 import useFetch from '../hooks/useFetch';
 
 const BlogPage = () => {
-    const movies = useFetch('https://api.themoviedb.org/3/movie/popular?api_key=d8d436ca878e548c5f0a4b2514f1e60b');
+    const {data: movies, loading} = useFetch('https://api.themoviedb.org/3/movie/popular?api_key=d8d436ca878e548c5f0a4b2514f1e60b');
 
     return (
         <Container>
             <StyledTitle withMarginTop>Check out our upcoming Movies</StyledTitle>
-            <MovieContainer>
-                {movies.map((movie) => (
-                    <MovieCard key={movie.id} {...movie} />
-                ))}
-            </MovieContainer>
+            {loading ? <StyledTitle withMarginTop>Loading Movies...</StyledTitle> : (
+                <MovieContainer>
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.id} {...movie} />
+                    ))}
+                </MovieContainer>
+            )}
         </Container>
     )
 }
